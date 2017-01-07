ee67550c2b7a25e6d68f2a12e9d9fcf0b09c6e6d
ZivKa <zivkalderon95@gmail.com>
2017-01-07 16:52:19 +0200

    Connect only to mongolav

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const User = require('./models/User');
 
 dotenv.load({ path: '.env.example' });
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
