16a52d2922592bfa401109f86cae3caddf4fdda2
zivkaziv <zivkalderon95@gmail.com>
2017-06-24 16:56:49 +0300

    How she like the sun bot

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function checkTwittsToRespod () {
        console.log('Start running.', chalk.red('✗'));
        //Get tweets
        // twitMessager.searchTweets('כביש החוף',10).then(function(tweets){
-       twitMessager.searchTweets('ים',10).then(function(tweets){
+       twitMessager.searchTweets('בריכה',10).then(function(tweets){
          //for every tweet
          tweets = tweets.filter((tweet, index, self) => self.findIndex((t) => {return t.user.id === tweet.user.id; }) === index)
          console.log('Found ' + tweets.length + ' tweets');
@@ -67,7 +67,8 @@ function handleSingleTweet(tweet){
 function replayTweetToUser(tweet,user){
   let tweetText = '';
   tweetText = 'הי @' + tweet.user.screen_name + ' איזה שמש... https://www.youtube.com/watch?v=ogxRmmUeM0o ';
-  tweetText += 'יה יה יה...';
+  // tweetText += 'יה יה יה...';
+  tweetText += 'איך היא אוהבת את השמש..';
 
   twitMessager.createAndPost(tweetText,tweet).then(function(){
    console.log('tweeting ' + tweetText);
